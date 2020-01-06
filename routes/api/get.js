358e943b9b2937ec216b6f8f6a542ca24bc60dbc
anishjana <anishjana007@gmail.com>
2020-01-06 17:10:54 +0530

    new

---
+++ routes/api/get.js
@@ -0,0 +1,78 @@
+const express= require('express');
+const axios = require('axios');
+
+const router = express.Router();
+const nse = require('./nse.js');
+const DomParser = require('dom-parser');
+const parser = new DomParser();
+// const fetch = require('node-fetch');
+
+
+
+
+router.get("/results", (req, res, next) => {
+    nse.getResults()
+      .then(function (server_response) {
+        const json_response = server_response.data.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": '); 
+        const myObject = JSON.parse(json_response);
+        res.send(myObject);
+      });
+  });
+
+router.post("/api", (req, res, next) => {
+  const Symbol_temp=req.body.symbol;
+  const Symbol = Symbol_temp.toUpperCase();
+  const Period_temp = req.body.period;
+  const Period = Period_temp.replace(/ /g,"%20");
+    nse.getListComp(Symbol,Period)
+      .then(function (server_response) {
+        const json_response = server_response.data.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": '); 
+        const myObject = JSON.parse(json_response);
+        res.send(myObject);
+        
+      });
+  });
+
+router.post("/actions", (req, res, next) => {
+  const Symbol=req.body.symbol;
+  // console.log(Symbol)
+  nse.actions(Symbol)
+      .then(function (json_response) {
+        const str = JSON.stringify(json_response.data)
+        const myObject = JSON.parse(str)
+        res.send(myObject)
+        
+      });
+  });
+  
+router.post("/announce", (req, res, next) => {
+  const Symbol=req.body.symbol;
+  // console.log(Symbol)
+  nse.announces(Symbol)
+      .then(function (json_response) {
+        const str = JSON.stringify(json_response.data)
+        const myObject = JSON.parse(str)
+        res.send(myObject)
+        
+      });
+  });
+
+  router.post('/corpinfo', (req,res,next)=> {
+    const Symbol=req.body.symbol;
+  // console.log(Symbol)
+  nse.corpinfo(Symbol)
+      .then(response => response.data)
+      .then(html => {
+        var doc = parser.parseFromString(html, "text/html");
+        res.send(doc.rawHTML);
+      })
+      .catch(function(err) {
+        console.log("Failed to fetch page: ", err);
+      });
+
+  });
+
+
+
+
+module.exports = router;
---
+++ routes/api/nse.js
@@ -0,0 +1,73 @@
+const axios = require('axios');
+
+function getResults() {
+  const url = `https://www1.nseindia.com/corporates/listDir/getListDirectEQ.jsp?Symbol=&Industry=&Period=Last%201%20month&symbol=&listingPeriod=Last%201%20month&segment=EQUITIES`
+  return axios.get(url,  {
+    headers: {
+      Referer: 'https://www1.nseindia.com/corporates/listDir/listDirectory.html',
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+    }).catch(err => {
+      console.log(err);
+    })
+}
+
+function getListComp(Symbol,Period) {
+  const list_url = `https://www.nseindia.com/corporates/listDir/getListDirectEQ.jsp?Symbol=${Symbol}&Industry=&Period=${Period}&symbol=${Symbol}&listingPeriod=${Period}&segment=EQUITIES`
+    return axios.get(list_url, {
+      headers: {
+        Referer: 'https://www1.nseindia.com/corporates/listDir/listDirectory.html',
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    }).catch(error => {
+        console.log(error);
+    });
+  }
+
+  function actions(Symbol) {
+    let url_action = `https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/companySnapshot/getCorporateActions${Symbol}.json`
+    return axios.get(url_action, {
+      headers: {
+        // Referer: 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=INFY&illiquid=0&smeFlag=0&itpFlag=0',
+        'X-Requested-With': 'XMLHttpRequest',
+        Host: 'www1.nseindia.com',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).catch(errors => {
+      console.log(errors)
+    })
+  }
+  
+  function announces(Symbol) {
+    let url_announce = `https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/companySnapshot/getAnnouncements${Symbol}.json`
+    return axios.get(url_announce, {
+      headers: {
+        // Referer: 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=INFY&illiquid=0&smeFlag=0&itpFlag=0',
+        'X-Requested-With': 'XMLHttpRequest',
+        Host: 'www1.nseindia.com',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).catch(errors => {
+      console.log(errors)
+    })
+  }
+  function corpinfo(Symbol) {
+    let url_info= `https://www1.nseindia.com/marketinfo/companyTracker/compInfo.jsp?symbol=${Symbol}&series=EQ`;
+    console.log(url_info)
+    return axios.get(url_info).catch(errors => {
+      console.log(errors)
+    })
+  }
+  
+
+var nse = {
+      getResults: getResults,
+      getListComp: getListComp,
+      actions: actions,
+      announces: announces,
+      corpinfo: corpinfo
+  }
+
+  module.exports = nse;
